895ace7dfa5fdf276ffb1be1088eb9fa10cebcae
akshat2635 <akshatjain2635@gmail.com>
2025-01-02 19:55:36 +0530

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   backend/src/controllers/messageController.js
    	modified:   backend/src/index.js
    	new file:   backend/src/models/message.model.js
    	new file:   backend/src/routes/message.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.js";
+import messageRoutes from "./routes/message.js";
 import { connectDB } from "./lib/db.js";
 
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
