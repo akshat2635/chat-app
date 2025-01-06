d72371d7784c2efbf9fedfbca09dcde46fb5942e
akshat2635 <akshatjain2635@gmail.com>
2025-01-07 02:29:12 +0530

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   backend/src/index.js
    	modified:   frontend/src/lib/axios.js
    	modified:   frontend/src/store/useAuthStore.js
    	modified:   package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,12 @@ import authRoutes from "./routes/auth.js";
 import messageRoutes from "./routes/message.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
-
+import path from "path";
 
 dotenv.config(); 
 
 const PORT = process.env.PORT;
+const __dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -19,6 +20,14 @@ app.use(cors({
 }))
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,6 +3,8 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client"
 
+const BASE_URL=import.meta.env.MODE ==="development"? "http://localhost:5000":"/";
+
 export const useAuthStore = create((set,get) => ({
   user: null,
   isSigningUp: false,
@@ -77,7 +79,7 @@ login: async (data)=>{
   connectSocket:()=>{
     const {user}=get();
     if(!user ||  get().socket?.connected) return;
-    const socket=io("http://localhost:5000",{
+    const socket=io(BASE_URL,{
       query:{
         userId:user._id
       }
